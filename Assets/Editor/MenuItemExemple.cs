4df41ceb6f14d759a045679e65a72bd1b48a16f7
Thomas Fontaine <thomas.fontaine@ubisoft.com>
2019-11-12 10:38:01 +0100

    Copy / pasting full project from local repo

---
+++ Assets/Editor/MenuItemExemple.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuItemExemple
+{
+    [MenuItem("Custom menu item/Create cube")]
+    public static void CreateCube()
+    {
+        Object.Instantiate(Resources.Load("PrefabCube"));
+    }
+}
---
+++ Assets/Scripts/PostProcessor/CustomPostprocessor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CustomPostprocessor : AssetPostprocessor
+{
+    // Before the texture is imported
+    public void OnPreprocessTexture()
+    {
+        if (assetPath.Contains("feedback"))
+        {
+            string targetFolder = "Assets/Textures/Feedback/";
+            string fileName = "feedback_test.png";
+
+            var validateMoveAsset = AssetDatabase.ValidateMoveAsset(assetPath, targetFolder + fileName);
+
+            // If it's an empty string, then no error occured.
+            if (string.IsNullOrEmpty(validateMoveAsset))
+            {
+                AssetDatabase.MoveAsset(assetPath, targetFolder + fileName);
+            }
+        }
+    }
+
+    // After the texture is imported
+    public void OnPostprocessTexture(Texture2D texture)
+    {
+        // Invert colors
+        if (assetPath.Contains("_invert_color"))
+        {
+            for (int m = 0; m < texture.mipmapCount; m++)
+            {
+                Color[] c = texture.GetPixels(m);
+                for (int i = 0; i < c.Length; i++)
+                {
+                    c[i].r = 1 - c[i].r;
+                    c[i].g = 1 - c[i].g;
+                    c[i].b = 1 - c[i].b;
+                }
+                texture.SetPixels(c, m);
+            }
+        }
+    }
+}
